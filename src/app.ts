6a347f967ce1dfebd9d3626f48b943fed24cfc50
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-05 22:21:37 -0300

    coupon post route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 require("dotenv").config();
 import express from "express";
 import { createConnection } from "typeorm";
-import { getCoupons } from "./controllers/coupons.controller";
+import { createCoupon, getCoupons } from "./controllers/coupons.controller";
 
 const app = express();
 createConnection();
 
 app.get("/coupons", getCoupons);
+app.post("/coupons", createCoupon);
 
 app.listen(process.env.PORT);
