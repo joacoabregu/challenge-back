b1962f5f19acd62acc7e8c2560ce14adb807de14
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-06 15:59:24 -0300

    coupons routes and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 require("dotenv").config();
 import express from "express";
 import { createConnection } from "typeorm";
-import { createCoupon, getCoupons, updateCoupon } from "./controllers/coupons.controller";
+import { createCoupon, deleteCoupon, getCoupons, updateCoupon, validateEmailCoupon } from "./controllers/coupons.controller";
 
 const app = express();
 createConnection();
 
 app.get("/coupons", getCoupons);
 app.post("/coupons", createCoupon);
-app.patch("/coupons", updateCoupon);
+app.patch("/coupons", validateEmailCoupon, updateCoupon);
+app.delete("/coupons", deleteCoupon);
+
 
 
 app.listen(process.env.PORT);
