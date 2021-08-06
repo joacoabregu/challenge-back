6da81b7d60f55de73bd3006d751823c70f2f2cf4
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-06 17:06:56 -0300

    createStore

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 require("dotenv").config();
 import express from "express";
 import { createConnection } from "typeorm";
-import { createCoupon, deleteCoupon, getCoupons, updateCoupon, validateEmailCoupon } from "./controllers/coupons.controller";
+import {
+  createCoupon,
+  deleteCoupon,
+  getCoupons,
+  updateCoupon,
+  validateEmailCoupon,
+} from "./controllers/coupons.controller";
+import { createStore, getStore, getStores } from "./controllers/stores.controller";
 
 const app = express();
 createConnection();
@@ -11,6 +18,8 @@ app.post("/coupons", createCoupon);
 app.patch("/coupons", validateEmailCoupon, updateCoupon);
 app.delete("/coupons", deleteCoupon);
 
+app.get("/stores", getStore, getStores);
+app.post("/stores", createStore);
 
 
 app.listen(process.env.PORT);
