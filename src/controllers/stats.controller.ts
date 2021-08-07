f283fd6c756519f4e7507406103627d4bba81010
Joaquin Abregu <abregujoaquin@gmail.com>
2021-08-07 16:02:13 -0300

    couponsExpiresByDay

--- src/controllers/stats.controller.ts
+++ src/controllers/stats.controller.ts
@@ -4,27 +4,35 @@ import { Request, Response } from "express";
 
 export const getStats = async (req: Request, res: Response) => {
   let repository = getRepository(Coupons);
+  try {
+    let coupons = await repository.find();
+    let assignedCoupons = coupons.filter((coupon) => {
+      return coupon.assigned_at !== null;
+    });
+    let notAssignedCoupons = coupons.length - assignedCoupons.length;
 
-  let coupons = await repository.find();
-  let assignedCoupons = coupons.filter((coupon) => {
-    return coupon.assigned_at !== null;
-  });
-  let notAssignedCoupons = coupons.length - assignedCoupons.length;
+    let couponsAssignedByDay = await repository
+      .createQueryBuilder("coupons")
+      .select("DATE(assigned_at)")
+      .distinct(true)
+      .getRawMany();
 
-  let assignedByDay = await repository
-    .createQueryBuilder("coupons")
-    .select("DATE(assigned_at)")
-    .distinct(true)
-    .getRawMany();
+    let couponsExpiresByDay = await repository
+      .createQueryBuilder("coupons")
+      .select("DATE(expires_at)")
+      .distinct(true)
+      .getRawMany();
 
-  console.log(assignedByDay);
+    let Stats = {
+      coupons_total: coupons.length,
+      assigned_coupons_total: assignedCoupons.length,
+      not_assigned_coupons_total: notAssignedCoupons,
+      assigned_by_day_total: couponsAssignedByDay.length - 1,
+      expires_by_day_total: couponsExpiresByDay.length,
+    };
 
-  let Stats = {
-    coupons_total: coupons.length,
-    assigned_coupons_total: assignedCoupons.length,
-    not_assigned_coupons_total: notAssignedCoupons,
-    assigned_by_day: assignedByDay.length - 1,
-  };
-
-  res.send(Stats);
+    res.send(Stats);
+  } catch (err) {
+    res.send({ message: "error", error: err.message });
+  }
 };
